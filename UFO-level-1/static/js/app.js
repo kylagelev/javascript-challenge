b02f54189a1011dcf25389070dec3d14af868d8a
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-03-19 16:42:28 -0700

    clean-up

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -11,37 +11,27 @@ var form = d3.select("#form");
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-// Complete the event handler function for the form
 function runEnter() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
 
-  // Prevent the page from refreshing
-  d3.event.preventDefault();
-  
-  // Select the input element and get the raw HTML node
-  var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
 
-  // Get the value property of the input element
-  var inputValue = inputElement.property("value");
+    console.log(inputValue);
 
-  console.log(inputValue);
+    var filteredData = alien.filter(alien => alien.datetime === inputValue)
 
-  var filteredData = alien.filter(alien => alien.datetime === inputValue);
+    console.log(filteredData);
+    var tbody = d3.select("tbody");
 
-  console.log(filteredData);
-  var tbody = d3.select("tbody");
-
-
-    // Step 1: Loop Through `data` and console.log each weather report object
     filteredData.forEach(function(alienReport) {
-        var row = tbody.append("tr");
+            var row = tbody.append("tr");
 
-        Object.entries(alienReport).forEach(function([key, value]) {
-            console.log(key, value);
-            var cell = row.append("td");
-            cell.text(value);
+            Object.entries(alienReport).forEach(function([key, value]) {
+                console.log(key, value);
+                var cell = row.append("td");
+                cell.text(value);
+            });
     });
- });
-
-  
 
 };
