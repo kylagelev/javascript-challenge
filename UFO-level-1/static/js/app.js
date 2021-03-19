daed3a00091ef9fdeedad3dd86386f4d17b5f019
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-03-19 16:06:52 -0700

    update
    uploading files, now need to do the actual javascript

---
+++ UFO-level-1/static/js/app.js
@@ -0,0 +1,33 @@
+// Assign the data from `data.js` to a descriptive variable
+var alien = data;
+
+// Select the button
+var button = d3.select("#button");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#form-control");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(people);
+
+  var filteredData = alien.filter(alien => alien.datetime === inputValue);
+
+  console.log(filteredData);
+
+};
