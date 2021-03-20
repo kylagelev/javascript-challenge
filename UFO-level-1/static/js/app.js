e03b0123d26a7e2ecaab0d3a87f97d3394756285
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-03-19 23:40:23 -0700

    update
    added clear function and when the search results do not match any data for it to display : "No search results found."

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -3,6 +3,8 @@ var alien = data;
 
 // Select the button
 var button = d3.select("#filter-btn");
+var clear = d3.select("#clear-btn");
+var tbody = d3.select('#table')
 
 // Select the form
 var form = d3.select("#form");
@@ -10,25 +12,37 @@ var form = d3.select("#form");
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
+clear.on("click", runClear);
+
+function runClear() {
+    d3.event.preventDefault();
+    console.log('Clear Data');
+    tbody.html('')
+};
 
 function runEnter() {
     d3.event.preventDefault();
+    var tbody = d3.select('#table')
     var inputElement = d3.select("#datetime");
-
     var inputValue = inputElement.property("value");
+    var filteredData = alien.filter(alien => alien.datetime === inputValue);
 
-    console.log(inputValue);
+    console.log(filteredData.length);
 
-    var filteredData = alien.filter(alien => alien.datetime === inputValue)
+    if (filteredData.length === 0){
+        console.log('No data');
+        var row = tbody.append("tr");
+        row.text('Search has no results');
+     };
 
     console.log(filteredData);
     var tbody = d3.select("tbody");
 
     filteredData.forEach(function(alienReport) {
-            var row = tbody.append("tr");
+            row = tbody.append("tr");
 
             Object.entries(alienReport).forEach(function([key, value]) {
-                console.log(key, value);
+                //console.log(key, value);
                 var cell = row.append("td");
                 cell.text(value);
             });
