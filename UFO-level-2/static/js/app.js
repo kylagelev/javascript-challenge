619d4219f5f2799a78817dd469aed846d7a7316f
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-03-20 20:42:27 -0700

    update
    really trying to do filtering on filtering...

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,6 +1,13 @@
 // Assign the data from `data.js` to a descriptive variable
 var alien = data;
 
+//setting variables
+// var datetime = alien.map(alien => alien.datetime);
+// var city = alien.map(alien => alien.city);
+// var state = alien.map(alien => alien.city);
+// var country = alien.map(alien => alien.city);
+// var shape = alien.map(alien => alien.city);
+
 // Select the button
 var button = d3.select("#filter-btn");
 var clear = d3.select("#clear-btn");
@@ -39,38 +46,120 @@ function runEnter() {
 
 
 
-    // if (alien.datetime === inputValuedate){
-    //     filteredData = alien.filter(alien =>
-    //             alien.datetime === inputValuedate
-    //          );
-    //     return filteredData
+    // if(inputValuedate !== null){
+    //     var fData = alien.filter(alien => 
+    //         alien.datetime === inputValuedate)
+    //     console.log(Data.length);
+
+    //     if (fData.length === 0){
+    //         console.log('No data');
+    //         var row = tbody.append("tr");
+    //         row.text('Search has no results');
+    //      }
+    
+    //      fData.forEach(function(alienReport) {
+    //         var row = tbody.append("tr");
+                
+    
+    //             Object.entries(alienReport).forEach(function([key, value]) {
+    //                 //console.log(key, value);
+    //                 var cell = row.append("td");
+    //                 cell.text(value);
+    //             });
+    //         }); 
     // };
 
+    // }
+
+    // if(inputValuecity !== null){
+
+    // }
+
+    // if(inputValuestate !== null){
+
+    // }
+
+    // if(inputValuecountry !== null){
+
+    // }
+
+    // if(inputValueshape !== null){
+
+    // }
+
+    // if(inputValuedate !== null & inputValuecity !== null){
+        
+    // }
+    // if(inputValuedate !== null & inputValuecity !== null & inputValuestate !== null){
+        
+    // }
+    // if(inputValuedate !== null & inputValuecity !== null & inputValuestate !== null & inputValuecountry !== null){
+        
+    // }
+    // if(inputValuedate !== null & inputValuecity !== null & inputValuestate !== null & inputValuecountry !== null & inputValueshape !== null){
+        
+    // }
 
 
-    var filteredData = alien.filter(alien => 
+    var fData = alien.filter(alien => 
         alien.datetime === inputValuedate ||
         alien.city === inputValuecity ||
         alien.state === inputValuestate ||
         alien.country === inputValuecountry || 
-        alien.shape === inputValueshape ||
-        alien.datetime === inputValuedate && alien.city === inputValuecity ||
-        alien.datetime === inputValuedate && alien.state === inputValuestate ||
-        alien.datetime === inputValuedate && alien.country === inputValuecountry ||
-        alien.datetime === inputValuedate && alien.shape === inputValueshape ||
-        alien.city === inputValuecity && alien.state === inputValuestate ||
-        alien.city === inputValuecity && alien.country === inputValuecountry ||
-        alien.city === inputValuecity && alien.shape === inputValueshape ||
-        alien.state === inputValuestate && alien.country === inputValuecountry ||
-        alien.state === inputValuestate && alien.shape === inputValueshape ||
-        alien.datetime === inputValuedate && alien.city === inputValuecity && alien.state === inputValuestate ||
-        alien.datetime === inputValuedate && alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry ||
-        alien.datetime === inputValuedate && alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry && alien.shape === inputValueshape ||
-        alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry && alien.shape === inputValueshape ||
-        alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry
-);
-
+        alien.shape === inputValueshape
+        // alien.datetime === inputValuedate && alien.city === inputValuecity ||
+        // alien.datetime === inputValuedate && alien.state === inputValuestate ||
+        // alien.datetime === inputValuedate && alien.country === inputValuecountry ||
+        // alien.datetime === inputValuedate && alien.shape === inputValueshape ||
+        // alien.city === inputValuecity && alien.state === inputValuestate ||
+        // alien.city === inputValuecity && alien.country === inputValuecountry ||
+        // alien.city === inputValuecity && alien.shape === inputValueshape ||
+        // alien.state === inputValuestate && alien.country === inputValuecountry ||
+        // alien.state === inputValuestate && alien.shape === inputValueshape ||
+        // alien.datetime === inputValuedate && alien.city === inputValuecity && alien.state === inputValuestate ||
+        // alien.datetime === inputValuedate && alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry ||
+        // alien.datetime === inputValuedate && alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry && alien.shape === inputValueshape ||
+        // alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry && alien.shape === inputValueshape ||
+        // alien.city === inputValuecity && alien.state === inputValuestate && alien.country === inputValuecountry
+    
+    );
 //this is not really working, I think what is happening is because the datetime is first, that even if I had an additional value because it's "or" it will only show one value over the double, multiple filter...
+
+
+    
+    var ffData = fData.filter(fData => 
+        fData.datetime === inputValuedate ||
+        fData.city === inputValuecity ||
+        fData.state === inputValuestate ||
+        fData.country === inputValuecountry || 
+        fData.shape === inputValueshape);
+
+    var fffData = ffData.filter(ffData => 
+        ffData.datetime === inputValuedate ||
+        ffData.city === inputValuecity ||
+        ffData.state === inputValuestate ||
+        ffData.country === inputValuecountry || 
+        ffData.shape === inputValueshape);
+
+    var ffffData = fffData.filter(fffData => 
+        fffData.datetime === inputValuedate ||
+        fffData.city === inputValuecity ||
+        fffData.state === inputValuestate ||
+        fffData.country === inputValuecountry || 
+        fffData.shape === inputValueshape);
+
+    var fffffData = ffffData.filter(ffffData =>  
+        ffffData.datetime === inputValuedate ||
+        ffffData.city === inputValuecity ||
+        ffffData.state === inputValuestate ||
+        ffffData.country === inputValuecountry || 
+        ffffData.shape === inputValueshape);
+
+
+
+
+
+
 
 
 
@@ -80,18 +169,18 @@ function runEnter() {
     //     alien.datetime === inputValuedate
     //  );
 
-    //have to figure out how to do one or two or whatever many filter available, otherwise, it will only show the data that is the same
-    //also want to make it so that when the filter doesn't match any data, it tell you instead of displaing data you know...
+   // have to figure out how to do one or two or whatever many filter available, otherwise, it will only show the data that is the same
+  //  also want to make it so that when the filter doesn't match any data, it tell you instead of displaing data you know...
 
-    console.log(filteredData.length);
+    console.log(fffffData.length);
 
-    if (filteredData.length === 0){
+    if (fffffData.length === 0){
         console.log('No data');
         var row = tbody.append("tr");
         row.text('Search has no results');
      }
 
-    filteredData.forEach(function(alienReport) {
+     fffffData.forEach(function(alienReport) {
         var row = tbody.append("tr");
             
 
