d9a820ea9c4d0df03baac085c7f2e5c9693032a8
Kyla <75227767+kylagelev@users.noreply.github.com>
2021-03-20 20:55:41 -0700

    update
    ok, so the push list length does work...

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -44,7 +44,30 @@ function runEnter() {
     var inputValuecountry = inputElementcountry.property("value");
     var inputValueshape = inputElementshape.property("value");
 
+    inputlist = []
+    
+    if(inputValuedate !== ''){
+    inputlist.push(inputValuedate)
+    }
+    if (inputValuecity !== ''){
+    inputlist.push(inputValuecity)
+    }
+    if (inputValuestate !== ''){
+    inputlist.push(inputValuestate)
+    }
+    if (inputValuecountry !== ''){
+    inputlist.push(inputValuecountry)
+    }
+    if (inputValueshape !== ''){
+    inputlist.push(inputValueshape)
+    }
 
+    console.log(inputlist.length)
+    console.log(inputValuedate)
+    console.log(inputValuecity)
+
+
+//I want to now decide what data to use depending on the inputlistlength
 
     // if(inputValuedate !== null){
     //     var fData = alien.filter(alien => 
@@ -126,41 +149,113 @@ function runEnter() {
 //this is not really working, I think what is happening is because the datetime is first, that even if I had an additional value because it's "or" it will only show one value over the double, multiple filter...
 
 
-    
-    var ffData = fData.filter(fData => 
-        fData.datetime === inputValuedate ||
-        fData.city === inputValuecity ||
-        fData.state === inputValuestate ||
-        fData.country === inputValuecountry || 
-        fData.shape === inputValueshape);
 
+    var ffData = fData.filter(fData => 
+    //datetime
+        fData.datetime === inputValuedate &
+        fData.city === inputValuecity ||
+        fData.datetime === inputValuedate &
+        fData.state === inputValuestate ||
+        fData.datetime === inputValuedate &
+        fData.country === inputValuecountry || 
+        fData.datetime === inputValuedate &
+        fData.shape === inputValueshape ||
+
+    //city
+        fData.city === inputValuecity &
+        fData.state === inputValuestate ||
+        fData.city === inputValuecity &
+        fData.country === inputValuecountry ||
+        fData.city === inputValuecity &
+        fData.shape === inputValueshape ||
+
+    //state
+        fData.state === inputValuestate &
+        fData.country === inputValuecountry ||
+        fData.state === inputValuestate &
+        fData.shape === inputValueshape ||
+
+    //country
+        fData.country === inputValuecountry &
+        fData.shape === inputValueshape
+        );
+
+
+    
     var fffData = ffData.filter(ffData => 
-        ffData.datetime === inputValuedate ||
-        ffData.city === inputValuecity ||
+    
+    //datetime
+        ffData.datetime === inputValuedate &
+        ffData.city === inputValuecity & 
         ffData.state === inputValuestate ||
-        ffData.country === inputValuecountry || 
-        ffData.shape === inputValueshape);
+
+        ffData.datetime === inputValuedate &
+        ffData.city === inputValuecity & 
+        ffData.country === inputValuecountry ||
+
+        ffData.datetime === inputValuedate &
+        ffData.city === inputValuecity & 
+        ffData.shape === inputValueshape ||
+
+    //city
+        ffData.city === inputValuecity &
+        ffData.state === inputValuestate &
+        ffData.country === inputValuecountry ||
+
+        ffData.city === inputValuecity &
+        ffData.state === inputValuestate &
+        ffData.shape === inputValueshape ||
+
+    //state
+        ffData.state === inputValuestate &
+        ffData.country === inputValuecountry &
+        ffData.shape === inputValueshape
+
+    //no shape
+    
+    );
 
     var ffffData = fffData.filter(fffData => 
-        fffData.datetime === inputValuedate ||
-        fffData.city === inputValuecity ||
-        fffData.state === inputValuestate ||
-        fffData.country === inputValuecountry || 
-        fffData.shape === inputValueshape);
 
-    var fffffData = ffffData.filter(ffffData =>  
-        ffffData.datetime === inputValuedate ||
-        ffffData.city === inputValuecity ||
-        ffffData.state === inputValuestate ||
-        ffffData.country === inputValuecountry || 
-        ffffData.shape === inputValueshape);
+    //datetime
+    fffData.datetime === inputValuedate &
+    fffData.city === inputValuecity & 
+    fffData.state === inputValuestate &
+    fffData.country === inputValuecountry ||
+
+    fffData.datetime === inputValuedate &
+    fffData.city === inputValuecity & 
+    fffData.state === inputValuestate &
+    fffData.shape === inputValueshape ||
+
+    //city
+    fffData.city === inputValuecity &
+    fffData.state === inputValuestate &
+    fffData.country === inputValuecountry &
+    fffData.shape === inputValueshape
+
+    //no state, no shape
+    );
+
+    var fffffData = ffffData.filter(ffffData => 
+    
+    //datetime
+        ffffData.datetime === inputValuedate &&
+        ffffData.city === inputValuecity &&
+        ffffData.state === inputValuestate &&
+        ffffData.country === inputValuecountry && 
+        ffffData.shape === inputValueshape
+    
+    //no city, no state, no shape
+        
+    );
 
 
 
 
 
 
-
+    if (inputlist.length === 1)
 
 
 
@@ -172,18 +267,17 @@ function runEnter() {
    // have to figure out how to do one or two or whatever many filter available, otherwise, it will only show the data that is the same
   //  also want to make it so that when the filter doesn't match any data, it tell you instead of displaing data you know...
 
-    console.log(fffffData.length);
+    console.log(filteredData.length);
 
-    if (fffffData.length === 0){
-        console.log('No data');
+     filteredData.forEach(function(alienReport) {
         var row = tbody.append("tr");
-        row.text('Search has no results');
-     }
 
-     fffffData.forEach(function(alienReport) {
-        var row = tbody.append("tr");
-            
-
+        if (filteredData.length === 0){
+            console.log('No data');
+            var row = tbody.append("tr");
+            row.text('Search has no results');
+         };
+        
             Object.entries(alienReport).forEach(function([key, value]) {
                 //console.log(key, value);
                 var cell = row.append("td");
